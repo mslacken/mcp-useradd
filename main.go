81b67b9503321ed09a6d1c64293ecd8534173478
Christian Goll <cgoll@suse.com>
2025-11-17 16:42:58 +0100

    added user add method

--- main.go
+++ main.go
@@ -14,6 +14,36 @@ import (
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
+// Input struct for the AddUser tool.
+type AddUserInput struct {
+	Username     string   `json:"username" jsonschema:"the username of the new account"`
+	BaseDir      string   `json:"base_dir,omitempty" jsonschema:"the base directory for the home directory of the new account"`
+	Comment      string   `json:"comment,omitempty" jsonschema:"the GECOS field of the new account"`
+	HomeDir      string   `json:"home_dir,omitempty" jsonschema:"the home directory of the new account"`
+	ExpireDate   string   `json:"expire_date,omitempty" jsonschema:"the expiration date of the new account"`
+	Inactive     int      `json:"inactive,omitempty" jsonschema:"the password inactivity period of the new account"`
+	Gid          string   `json:"gid,omitempty" jsonschema:"the name or ID of the primary group of the new account"`
+	Groups       []string `json:"groups,omitempty" jsonschema:"the list of supplementary groups of the new account"`
+	SkelDir      string   `json:"skel_dir,omitempty" jsonschema:"the alternative skeleton directory"`
+	CreateHome   bool     `json:"create_home,omitempty" jsonschema:"create the user's home directory"`
+	NoCreateHome bool     `json:"no_create_home,omitempty" jsonschema:"do not create the user's home directory"`
+	NoUserGroup  bool     `json:"no_user_group,omitempty" jsonschema:"do not create a group with the same name as the user"`
+	NonUnique    bool     `json:"non_unique,omitempty" jsonschema:"allow to create users with duplicate (non-unique) UID"`
+	Password     string   `json:"password,omitempty" jsonschema:"the encrypted password of the new account"`
+	System       bool     `json:"system,omitempty" jsonschema:"create a system account"`
+	Shell        string   `json:"shell,omitempty" jsonschema:"the login shell of the new account"`
+	Uid          int      `json:"uid,omitempty" jsonschema:"the user ID of the new account"`
+	UserGroup    bool     `json:"user_group,omitempty" jsonschema:"create a group with the same name as the user"`
+	SelinuxUser  string   `json:"selinux_user,omitempty" jsonschema:"the specific SEUSER for the SELinux user mapping"`
+	SelinuxRange string   `json:"selinux_range,omitempty" jsonschema:"the specific MLS range for the SELinux user mapping"`
+}
+
+// Output struct for the AddUser tool.
+type AddUserOutput struct {
+	Success bool   `json:"success" jsonschema:"whether the user was added successfully"`
+	Message string `json:"message" jsonschema:"a message indicating the result of the operation"`
+}
+
 // User struct represents a single user account.
 type User struct {
 	Username     string   `json:"username"`
@@ -170,6 +200,78 @@ func getGroups() ([]Group, error) {
 	return groups, nil
 }
 
+func AddUser(ctx context.Context, req *mcp.CallToolRequest, input AddUserInput) (
+	*mcp.CallToolResult, AddUserOutput, error,
+) {
+	slog.Info("AddUser tool called")
+	args := []string{}
+	if input.BaseDir != "" {
+		args = append(args, "-b", input.BaseDir)
+	}
+	if input.Comment != "" {
+		args = append(args, "-c", input.Comment)
+	}
+	if input.HomeDir != "" {
+		args = append(args, "-d", input.HomeDir)
+	}
+	if input.ExpireDate != "" {
+		args = append(args, "-e", input.ExpireDate)
+	}
+	if input.Inactive != 0 {
+		args = append(args, "-f", strconv.Itoa(input.Inactive))
+	}
+	if input.Gid != "" {
+		args = append(args, "-g", input.Gid)
+	}
+	if len(input.Groups) > 0 {
+		args = append(args, "-G", strings.Join(input.Groups, ","))
+	}
+	if input.SkelDir != "" {
+		args = append(args, "-k", input.SkelDir)
+	}
+	if input.CreateHome {
+		args = append(args, "-m")
+	}
+	if input.NoCreateHome {
+		args = append(args, "-M")
+	}
+	if input.NoUserGroup {
+		args = append(args, "-N")
+	}
+	if input.NonUnique {
+		args = append(args, "-o")
+	}
+	if input.Password != "" {
+		args = append(args, "-p", input.Password)
+	}
+	if input.System {
+		args = append(args, "-r")
+	}
+	if input.Shell != "" {
+		args = append(args, "-s", input.Shell)
+	}
+	if input.Uid != 0 {
+		args = append(args, "-u", strconv.Itoa(input.Uid))
+	}
+	if input.UserGroup {
+		args = append(args, "-U")
+	}
+	if input.SelinuxUser != "" {
+		args = append(args, "-Z", input.SelinuxUser)
+	}
+	args = append(args, input.Username)
+
+	cmd := exec.Command("useradd", args...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	err := cmd.Run()
+	if err != nil {
+		return nil, AddUserOutput{Success: false, Message: out.String()}, err
+	}
+	return nil, AddUserOutput{Success: true, Message: out.String()}, nil
+}
+
 func main() {
 	listenAddr := flag.String("http", "", "address for http transport, defaults to stdio")
 	flag.Parse()
@@ -179,6 +281,10 @@ func main() {
 		Name:        "ListUsers",
 		Description: "A tool to list the users on the system",
 	}, ListUsers)
+	mcp.AddTool(server, &mcp.Tool{
+		Name:        "AddUser",
+		Description: "A tool to add a new user to the system",
+	}, AddUser)
 
 	if *listenAddr == "" {
 		// Run the server on the stdio transport.
