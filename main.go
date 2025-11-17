bbc7cea13cde1a2ba6f08b40fbb94751c069b4b0
Christian Goll <cgoll@suse.com>
2025-11-17 15:32:27 +0100

    better user list

--- main.go
+++ main.go
@@ -16,18 +16,29 @@ import (
 
 // User struct represents a single user account.
 type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	UID      int    `json:"uid"`
-	GID      int    `json:"gid"`
-	Comment  string `json:"comment"`
-	Home     string `json:"home"`
-	Shell    string `json:"shell"`
+	Username     string   `json:"username"`
+	Password     string   `json:"password"`
+	UID          int      `json:"uid"`
+	GID          int      `json:"gid"`
+	Comment      string   `json:"comment"`
+	Home         string   `json:"home"`
+	Shell        string   `json:"shell"`
+	IsSystemUser bool     `json:"is_system_user"`
+	Groups       []string `json:"groups"`
+}
+
+// Group struct represents a single group.
+type Group struct {
+	Name     string   `json:"name"`
+	Password string   `json:"password"`
+	GID      int      `json:"gid"`
+	Members  []string `json:"members"`
 }
 
 // Output struct for the ListUsers tool.
 type ListUsersOutput struct {
-	Users []User `json:"users" jsonschema:"the list of users on the system"`
+	Users  []User  `json:"users" jsonschema:"the list of users on the system"`
+	Groups []Group `json:"groups" jsonschema:"the list of groups on the system"`
 }
 
 // Input struct for the ListUsers tool.
@@ -56,16 +67,52 @@ func ListUsers(ctx context.Context, req *mcp.CallToolRequest, _ ListUsersInput) 
 		uid, _ := strconv.Atoi(parts[2])
 		gid, _ := strconv.Atoi(parts[3])
 		users = append(users, User{
-			Username: parts[0],
-			Password: parts[1],
-			UID:      uid,
-			GID:      gid,
-			Comment:  parts[4],
-			Home:     parts[5],
-			Shell:    parts[6],
+			Username:     parts[0],
+			Password:     parts[1],
+			UID:          uid,
+			GID:          gid,
+			Comment:      parts[4],
+			Home:         parts[5],
+			Shell:        parts[6],
+			IsSystemUser: gid < 1000,
+			Groups:       []string{},
 		})
 	}
-	return nil, ListUsersOutput{Users: users}, nil
+
+	cmd = exec.Command("getent", "group")
+	var groupOut bytes.Buffer
+	cmd.Stdout = &groupOut
+	err = cmd.Run()
+	if err != nil {
+		return nil, ListUsersOutput{}, err
+	}
+	var groups []Group
+	groupScanner := bufio.NewScanner(&groupOut)
+	for groupScanner.Scan() {
+		line := groupScanner.Text()
+		parts := strings.Split(line, ":")
+		if len(parts) != 4 {
+			continue
+		}
+		gid, _ := strconv.Atoi(parts[2])
+		members := strings.Split(parts[3], ",")
+		groups = append(groups, Group{
+			Name:     parts[0],
+			Password: parts[1],
+			GID:      gid,
+			Members:  members,
+		})
+		groupName := parts[0]
+		for _, member := range members {
+			for i, user := range users {
+				if user.Username == member {
+					users[i].Groups = append(users[i].Groups, groupName)
+				}
+			}
+		}
+	}
+
+	return nil, ListUsersOutput{Users: users, Groups: groups}, nil
 }
 
 func main() {
